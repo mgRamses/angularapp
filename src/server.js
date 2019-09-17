cdd27e89612e4e2bcd1835610caee4d7324cb537
Ram <mg.ramses@gmail.com>
2019-09-17 09:23:58 -0500

    updates to heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/itswa-app'));
+app.use(express.static(__dirname + '/itswa-app'));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/itswa-app/index.html'));
+    res.sendFile(path.join(__dirname + '/itswa-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
